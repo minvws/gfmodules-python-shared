0a59caa0030600f832ea06a484dc2e88b28b6e2f
Rick Lambrechts <rick@rl-webdiensten.nl>
2024-08-16 12:03:08 +0200

    Add page schema and base model schema

---
+++ gfmodules_python_shared/schema/pagination/schema.py
@@ -0,0 +1,16 @@
+from typing import List, TypeVar, Generic
+
+from gfmodules_python_shared.schema.base_model_schema import BaseModelConfig
+
+T = TypeVar("T")
+
+
+class Page(BaseModelConfig, Generic[T]):
+    """
+    Schema for pagination of entities in the application.
+    """
+
+    items: List[T]
+    limit: int
+    offset: int
+    total: int
