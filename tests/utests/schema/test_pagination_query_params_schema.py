545d8211769f5e9510128cce07fa092504b87d53
Rick Lambrechts <rick@rl-webdiensten.nl>
2024-08-20 17:47:21 +0200

    PaginationQueryParams based on Query of FastAPI
    So that the query params are loaded from the query by FastAPI

--- tests/utests/schema/test_pagination_query_params_schema.py
+++ tests/utests/schema/test_pagination_query_params_schema.py
@@ -4,31 +4,31 @@ from pydantic import ValidationError
 from gfmodules_python_shared.schema.pagination.pagination_query_params_schema import PaginationQueryParams
 
 
-def test_pagination_query_params_defaults() -> None:
+def test_pagination_params_defaults() -> None:
     params = PaginationQueryParams()
 
     assert params.limit == 10
     assert params.offset == 0
 
 
-def test_pagination_query_params_custom_values() -> None:
+def test_pagination_params_custom_values() -> None:
     params = PaginationQueryParams(limit=20, offset=5)
 
     assert params.limit == 20
     assert params.offset == 5
 
 
-def test_pagination_query_params_invalid_limit() -> None:
+def test_pagination_params_invalid_limit() -> None:
     with pytest.raises(ValidationError) as excinfo:
         PaginationQueryParams(limit=0)
-    assert "limit must be greater than 0" in str(excinfo.value)
+    assert "Input should be greater than 0" in str(excinfo.value)
 
     with pytest.raises(ValidationError) as excinfo:
         PaginationQueryParams(limit=-10)
-    assert "limit must be greater than 0" in str(excinfo.value)
+    assert "Input should be greater than 0" in str(excinfo.value)
 
 
-def test_pagination_query_params_invalid_offset() -> None:
+def test_pagination_params_invalid_offset() -> None:
     with pytest.raises(ValidationError) as excinfo:
         PaginationQueryParams(offset=-1)
-    assert "offset must be greater than or equal to 0" in str(excinfo.value)
+    assert "Input should be greater than or equal to 0" in str(excinfo.value)
