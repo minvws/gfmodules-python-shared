6a55fa79533c265a2daeaa8793407bc4795f4e0d
Sharlon N. Regales <sharlonregales@gmail.com>
2024-10-09 08:43:14 +0200

    Sync outgoing entity or entities
    This is done only for keeping the status quot.
    It will be removed in the future database-linked objects are not allow
    to leave pass the service scope

--- gfmodules_python_shared/session/session_manager.py
+++ gfmodules_python_shared/session/session_manager.py
@@ -1,16 +1,14 @@
-from functools import wraps
 import inspect
+from collections.abc import Sequence
+from functools import wraps
+from typing import Any, Callable, ParamSpec, TypeVar
+
 import inject
+from sqlalchemy.orm import DeclarativeBase, Session, attributes, sessionmaker
 
-from typing import Callable, Type, TypeVar, ParamSpec, Any
-
-from sqlalchemy.orm import Session, sessionmaker
-
-from gfmodules_python_shared.repository import T
 from gfmodules_python_shared.repository.base import GenericRepository
 
-
-R = TypeVar("R")
+T = TypeVar("T")
 P = ParamSpec("P")
 
 
@@ -19,25 +17,44 @@ def get_repository() -> Any:
     return None
 
 
-def session_manager(func: Callable[P, R]) -> Callable[P, R]:
-    """
-    This decorator requests, injects and cleans your session for the given service operation context.
+def sync_value_with_database(session: Session, value: Any) -> None:
+    if isinstance(value, DeclarativeBase) and session.identity_map.contains_state(
+        attributes.instance_state(value)
+    ):
+        session.refresh(value)
+    elif isinstance(value, Sequence):
+        for e in value:
+            sync_value_with_database(session, e)
 
-    The session is encapsulated by this decorator, thus will not be exposed to the service operation.
 
-    Use of type annotation is vital, GenericRepository subclass parameters are instantiated with the requested session, and injected in the service operation signature
+def session_manager(service: Callable[P, T]) -> Callable[P, T]:
     """
-    @wraps(func)
-    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+    This decorator requests, injects and cleans your session for the given service
+    operation context.
+
+    The session is encapsulated by this decorator, thus will not be exposed to the
+    service operation. Use of type annotation is vital, GenericTepository subclass
+    parameters are instantiated with the requested session, and injected in the service
+    operation signature.
+
+    return value is synced with the database before sent to caller.
+    """
+
+    @wraps(service)
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
         session_maker = inject.instance(sessionmaker[Session])
 
         with session_maker() as session:
-            kwargs |= { # type: ignore
+            kwargs |= {  # type: ignore
                 parameter.name: parameter.annotation(session)
-                for parameter in inspect.signature(func).parameters.values()
-                if issubclass(parameter.annotation, GenericRepository)
+                for parameter in inspect.signature(service).parameters.values()
+                if inspect.isclass(parameter.annotation)
+                and issubclass(parameter.annotation, GenericRepository)
+                and parameter.default is None
             }
             with session.begin():
-                return func(*args, **kwargs)
+                value = service(*args, **kwargs)
+            sync_value_with_database(session, value)
+        return value
 
     return wrapper
